2f42afbf3b711f3854a7ced79467eba069eacda3
jangwoojoo315 <55902008+jangwoojoo315@users.noreply.github.com>
2021-03-21 18:34:52 +0900

    constellation 첫 깃연동 테스트

---
+++ constellation/src/main/java/com/jang/constellation/ConstellationController.java
@@ -0,0 +1,18 @@
+package com.jang.constellation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ConstellationController {
+	@RequestMapping(value="/")
+	public String Constellation(){
+		return "main";
+	}
+	@RequestMapping(value="/selectweather")
+	public String SelectWeather() {
+		return "selectweather";
+	}
+}
---
+++ constellation/src/test/java/com/jang/constellation/ConstellationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jang.constellation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstellationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
